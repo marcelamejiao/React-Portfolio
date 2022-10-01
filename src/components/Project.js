da0352e9744c4e05a532e615c13ecad121f25ddc
Marcela Mejia <mejiao24@hotmail.com>
2022-10-02 00:43:25 +1000

    Implement portfolio projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,14 @@
-function Project () {
+function Project ({title, github, deployedLink, image}) {
     return (
         <div>
-            Project
+            <h2>{title}</h2>
+            <div>
+            <a href={github} alt={title + " Github link"}>Link to Github</a>
+            </div>
+            <div>
+            <a href={deployedLink} alt={title + " Deployed link"}>Deployed link</a>
+            </div>
+            <img src={image} alt={title + " Image link"} />
         </div>
     );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,51 @@
 import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
+import Project from '../Project';
 
 function Portfolio ({currentPage, handlePageChange}) {
     return (
         <div>
             <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-            Portfolio
+            <section>
+                <h1>Projects</h1>
+                <Project 
+                    title="Bootcamp Portfolio" 
+                    github="https://github.com/marcelamejiao/Bootcamp-Portfolio" 
+                    deployedLink="https://marcelamejiao.github.io/Bootcamp-Portfolio/"
+                    image=""
+                />
+                <Project 
+                    title="Text Editor" 
+                    github="https://github.com/marcelamejiao/Text-Editor" 
+                    deployedLink="https://salty-cove-60850.herokuapp.com"
+                    image=""
+                />
+                <Project 
+                    title="Weather Dashboard" 
+                    github="https://github.com/marcelamejiao/Weather-Dashboard" 
+                    deployedLink="https://marcelamejiao.github.io/Weather-Dashboard/"
+                    image=""
+                />
+                <Project 
+                    title="Pawfect Match" 
+                    github="https://github.com/marcelamejiao/pawfect-match" 
+                    deployedLink="https://pawfect---match.herokuapp.com"
+                    image=""
+                />
+                <Project 
+                    title="Umbrella Address Book" 
+                    github="https://github.com/marcelamejiao/Umbrella-Address-Book" 
+                    deployedLink="https://marcelamejiao.github.io/Umbrella-Address-Book/"
+                    image=""
+                />
+                <Project 
+                    title="Tech Blog" 
+                    github="https://github.com/marcelamejiao/Tech-blog" 
+                    deployedLink="https://el-tech-blog-mar.herokuapp.com"
+                    image=""
+                />
+            </section>
             <Footer /> 
         </div>
     );
