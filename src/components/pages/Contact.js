e48715be045f9f68cc39d156c372d82312553abb
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 13:25:46 +1100

    Implement contact section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -6,7 +6,17 @@ function Contact ({currentPage, handlePageChange}) {
     return (
         <div>
             <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-            Contact
+            <h1>Contact</h1>
+            <form>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" />
+                <label for="email">Email:</label>
+                <input type="text" id="email" name="email" />
+                <label for="message">Message:</label>
+                <textarea id="message" name="message" rows="4" cols="50"></textarea>
+                <input type="submit" value="Send" />
+            </form> 
+
             <Footer />
         </div>
     );
