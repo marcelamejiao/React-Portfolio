2f17c568768c907efa218ba811ae5c5ebe163a59
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 16:39:01 +1100

    Implement icon links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Footer () {
     return (
         <div>
-            Footer
+            <section class="content">            
+                <a href="https://github.com/marcelamejiao" target="_blank" class="fa fa-github"> Github</a>
+
+                <a href="https://www.linkedin.com/in/wendy-marcela-mejía-ortiz-80b10a157/" target="_blank" class="fa fa-linkedin"> Linkedin</a>
+            
+                <a href="https://twitter.com/?lang=en" target="_blank" class="fa fa-twitter"> Twitter</a>
+            </section>
         </div>
     );
 }
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -27,6 +27,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
       {renderPage()}
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
