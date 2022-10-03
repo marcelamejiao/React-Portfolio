144aeb78487eaaefca936c0b74105e72f6e468a4
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 21:12:57 +1100

    Move third party libraries to index.html

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -27,14 +27,7 @@ export default function PortfolioContainer() {
 
   return (
     <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
       {renderPage()}
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF"
-        crossOrigin="anonymous"></script>
     </div>
   );
 }
