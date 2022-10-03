fbc695f5bdfdee34c3f719aae8f83c7fdae6e8c2
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 13:49:56 +1100

    Implement Resume section

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,7 +6,28 @@ function Resume ({currentPage, handlePageChange}) {
     return (
         <div>
             <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-            Resume
+            <h1>Resume</h1>
+
+            <p>Download my <a href="/resume.docx" download>resume</a></p>
+
+            <ul>
+                <li>React.js</li>
+                <li>Object-Oriented Programming (OOP)</li>
+                <li>MongoDB</li>
+                <li>Mongoose ODM</li>
+                <li>Express.js</li>
+                <li>Node.js</li>
+                <li>MySQLs</li>
+                <li>Sequelize</li>
+                <li>JavaScript</li>
+                <li>Cascading Style Sheets (CSS)</li>
+                <li>jQuery</li>
+                <li>HTML5</li>
+                <li>Bootstrap</li>
+                <li>Moment.js</li>
+                <li>GitHub</li>
+            </ul>
+
             <Footer />
         </div>
     );
