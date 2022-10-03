4ca019027f570ca6e14db018b0485121f7cdad28
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 22:28:06 +1100

    Style projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,6 @@
 function Project ({title, github, deployedLink, image}) {
     return (
-        <div>
+        <div className='project'>
             <h2>{title}</h2>
             <div>
             <a href={github} alt={title + " Github link"}>Link to Github</a>
@@ -8,7 +8,7 @@ function Project ({title, github, deployedLink, image}) {
             <div>
             <a href={deployedLink} alt={title + " Deployed link"}>Deployed link</a>
             </div>
-            <img src={image} alt={title + " Image link"} />
+            <img className="w-100 h-50" src={image} alt={title + " Image link"} />
         </div>
     );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -5,46 +5,65 @@ import Project from '../Project';
 
 function Portfolio ({currentPage, handlePageChange}) {
     return (
-        <div>
+        <div className="container">
             <Header currentPage={currentPage} handlePageChange={handlePageChange} />
             <section>
                 <h1>Projects</h1>
-                <Project 
-                    title="Bootcamp Portfolio" 
-                    github="https://github.com/marcelamejiao/Bootcamp-Portfolio" 
-                    deployedLink="https://marcelamejiao.github.io/Bootcamp-Portfolio/"
-                    image=""
-                />
-                <Project 
-                    title="Text Editor" 
-                    github="https://github.com/marcelamejiao/Text-Editor" 
-                    deployedLink="https://salty-cove-60850.herokuapp.com"
-                    image=""
-                />
-                <Project 
-                    title="Weather Dashboard" 
-                    github="https://github.com/marcelamejiao/Weather-Dashboard" 
-                    deployedLink="https://marcelamejiao.github.io/Weather-Dashboard/"
-                    image=""
-                />
-                <Project 
-                    title="Pawfect Match" 
-                    github="https://github.com/marcelamejiao/pawfect-match" 
-                    deployedLink="https://pawfect---match.herokuapp.com"
-                    image=""
-                />
-                <Project 
-                    title="Umbrella Address Book" 
-                    github="https://github.com/marcelamejiao/Umbrella-Address-Book" 
-                    deployedLink="https://marcelamejiao.github.io/Umbrella-Address-Book/"
-                    image=""
-                />
-                <Project 
-                    title="Tech Blog" 
-                    github="https://github.com/marcelamejiao/Tech-blog" 
-                    deployedLink="https://el-tech-blog-mar.herokuapp.com"
-                    image=""
-                />
+                <div>
+                    <div className="row">
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Bootcamp Portfolio" 
+                            github="https://github.com/marcelamejiao/Bootcamp-Portfolio" 
+                            deployedLink="https://marcelamejiao.github.io/Bootcamp-Portfolio/"
+                            image="/assets/images/bootcamp-portfolio.png"
+                        />
+                        </div>
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Text Editor" 
+                            github="https://github.com/marcelamejiao/Text-Editor" 
+                            deployedLink="https://salty-cove-60850.herokuapp.com"
+                            image="/assets/images/text-editor.png"
+                        />
+                        </div>
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Weather Dashboard" 
+                            github="https://github.com/marcelamejiao/Weather-Dashboard" 
+                            deployedLink="https://marcelamejiao.github.io/Weather-Dashboard/"
+                            image="/assets/images/weather-dashboard.png"
+                        />
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Pawfect Match" 
+                            github="https://github.com/marcelamejiao/pawfect-match" 
+                            deployedLink="https://pawfect---match.herokuapp.com"
+                            image="/assets/images/pawfect-match.png"
+                        />
+                        </div>
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Umbrella Address Book" 
+                            github="https://github.com/marcelamejiao/Umbrella-Address-Book" 
+                            deployedLink="https://marcelamejiao.github.io/Umbrella-Address-Book/"
+                            image="/assets/images/umbrella-address-book.png"
+                        />
+                        </div>
+                        <div className="col-sm-4">
+                        <Project 
+                            title="Tech Blog" 
+                            github="https://github.com/marcelamejiao/Tech-blog" 
+                            deployedLink="https://el-tech-blog-mar.herokuapp.com"
+                            image="/assets/images/tech-blog.png"
+                        />
+                        </div>
+                    </div>
+                </div>
             </section>
             <Footer /> 
         </div>
