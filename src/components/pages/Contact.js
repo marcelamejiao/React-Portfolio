e7facf2b98b2fa1cd2fe35d52dd6788983c1be0f
Marcela Mejia <mejiao24@hotmail.com>
2022-10-03 20:38:28 +1100

    Implement validation for contact content

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
 
 function Contact ({currentPage, handlePageChange}) {
+    
+    let [error, setError] = useState('');
+
+    const validateName = (e) => {
+        e.preventDefault();
+
+        const input = e.target;
+
+        if (input.value === '') {
+            setError ('Please enter a name');
+        } else {
+            setError('');
+        }
+    };
+
+    const validateEmail = (e) => {
+        e.preventDefault();
+
+        const input = e.target;
+
+        if (input.value === '' || !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(input.value)) {
+            setError ('Please enter a valid email');
+        } else {
+            setError('');
+        }
+    };
+
+    const validateMessage = (e) => {
+        e.preventDefault();
+
+        const input = e.target;
+
+        if (input.value === '') {
+            setError ('Please enter a message');
+        } else {
+            setError('');
+        }
+    };
+
     return (
         <div>
             <Header currentPage={currentPage} handlePageChange={handlePageChange} />
             <h1>Contact</h1>
             <form>
-                <label for="name">Name:</label>
-                <input type="text" id="name" name="name" />
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" />
-                <label for="message">Message:</label>
-                <textarea id="message" name="message" rows="4" cols="50"></textarea>
-                <input type="submit" value="Send" />
+                <p>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" name="name" onBlur={validateName} />
+                </p>
+                <p>
+                    <label htmlFor="email">Email:</label>
+                    <input type="text" id="email" name="email" onBlur={validateEmail} />
+                </p>
+                <p>
+                    <label htmlFor="message">Message:</label>
+                    <textarea id="message" name="message" rows="4" cols="50" onBlur={validateMessage}></textarea>
+                </p>
+                <p>
+                    {error}
+                </p>
+                <p>
+                    <input type="submit" value="Send" />
+                </p>
             </form> 
 
             <Footer />
