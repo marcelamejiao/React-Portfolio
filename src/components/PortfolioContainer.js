be2fa21a16931665cd39c3108283b47e1207ce5e
Marcela Mejia <mejiao24@hotmail.com>
2022-10-04 23:50:03 +1100

    Add logic for the user to be able to press the back button

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -6,8 +6,24 @@ import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
+  // If the user accesses the page with a hash, start on that page
+  const getPageFromHash = () => {
+    switch (window.location.hash) {
+      case '#contact':
+        return 'Contact';
+      case '#portfolio':
+        return 'Portfolio';
+      case '#resume':
+        return 'Resume';
+      default:
+        return 'About';
+    }
+  };
+
+  const startCurrentPage = getPageFromHash();
+
   // Store the current page in the state
-  const [currentPage, setCurrentPage] = useState('About');
+  const [currentPage, setCurrentPage] = useState(startCurrentPage);
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
@@ -24,6 +40,9 @@ export default function PortfolioContainer() {
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
+
+  // If the user presses the back button, render the old page
+  window.addEventListener('hashchange', () => setCurrentPage(getPageFromHash()))
 
   return (
     <div>
